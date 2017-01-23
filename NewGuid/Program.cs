1816f63902b3e291023ba8d03a5c54bcbe0346e9
Nathan Blair <nblair@cliexa.com>
2017-01-22 19:24:38 -0700

    Initial commit

--- NewGuid/Program.cs
+++ NewGuid/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace NewGuid
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        private static void Main(string[] args)
         {
+            var guid = Guid.NewGuid();
+            var format = args.Length > 0 ? args[0] : "N";
+
+            try
+            {
+                var formattedGuid = guid.ToString(format);
+
+                Clipboard.SetText(formattedGuid);
+
+                Console.WriteLine($"Guid {formattedGuid} has been copied to clipboard.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed: {e}");
+            }
         }
     }
 }
